7666e64393e06c5078c8127008e0c0ab61647c8e
Jimmy Lazo <astrojlazo@gmail.com>
2017-07-25 04:28:28 -0500

    Update readcsv.py

--- readcsv.py
+++ readcsv.py
@@ -1,10 +1,10 @@
-# Script to read a standard csv file
+""" Script to read a standard csv file """
 import csv
 # Opens Test.csv to be read with 'r'
 with open('test.csv', 'r') as csvfile:
     # The default delimiter is a comma
-    creader = csv.reader(csvfile, delimiter=',')
-    for row in creader:
+    CREADER = csv.reader(csvfile, delimiter=',')
+    for row in CREADER:
         # Takes and prints each row to console adding a
         # comma where the delimiter (comma in this case) was.
         print ','.join(row)
